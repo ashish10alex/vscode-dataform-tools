1b129e677d948d630ab6bd95cb3cce05a31ff704
ashish.alex@gmail.com <ashish.alex10@gmail.com>
2024-07-29 20:24:05 +0100

    chore: add TOODs

--- src/views/register-center-panel.ts
+++ src/views/register-center-panel.ts
@@ -60,6 +60,7 @@ export class CenterPanel {
                 const webview = panel.webview;
                 if (panel.visible) {
                     let dataformTreeMetadata = await generateDependancyTreeMetada();
+                    // TODO: Use tableName of the last active editor when possible to use as treeRoot
                     // TODO: check if treeRoot still exsists in dataformTreeMetadata
                     await webview.postMessage({ "dataformTreeMetadata": dataformTreeMetadata, "treeRoot": treeRoot, "direction": direction });
                     if (this.centerPanel) {
