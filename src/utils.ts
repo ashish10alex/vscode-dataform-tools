f8f95954f021d66325ae08e80727d1b73c0cafbc
ashish.alex@gmail.com <ashish.alex10@gmail.com>
2024-09-20 14:53:26 +0100

    fix: dry run stats not showing for js files

--- src/utils.ts
+++ src/utils.ts
@@ -924,7 +924,7 @@ export async function compiledQueryWtDryRun(document: vscode.TextDocument,  diag
     ]);
 
     if (dryRunResult.error.hasError || preOpsDryRunResult.error.hasError || postOpsDryRunResult.error.hasError) {
-        if (!sqlxBlockMetadata){
+        if (!sqlxBlockMetadata && extension === ".sqlx"){
             vscode.window.showErrorMessage("Could not parse sqlx file");
             return;
         }
@@ -936,7 +936,9 @@ export async function compiledQueryWtDryRun(document: vscode.TextDocument,  diag
             offSet = assertionQueryOffset;
         }
 
-        setDiagnostics(document, dryRunResult.error, preOpsDryRunResult.error, postOpsDryRunResult.error, compiledSqlFilePath, diagnosticCollection, sqlxBlockMetadata, offSet);
+        if (sqlxBlockMetadata){
+            setDiagnostics(document, dryRunResult.error, preOpsDryRunResult.error, postOpsDryRunResult.error, compiledSqlFilePath, diagnosticCollection, sqlxBlockMetadata, offSet);
+        }
         return;
     }
     let combinedTableIds = "";
