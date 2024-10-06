eda9398e8b8a0be79f8c6a7e5a46c3e1dcbf1458
Ashish Alex <ashish.alex10@gmail.com>
2024-10-06 11:41:05 +0100

    feat: Navbar to show compiled query in results preview webview (#29)

--- media/js/showQueryResults.js
+++ media/js/showQueryResults.js
@@ -1,4 +1,61 @@
-// const vscode = acquireVsCodeApi();
+const vscode = acquireVsCodeApi();
+
+document.getElementById("cancelBigQueryJobButton").disabled = true;
+
+// Get all navigation links
+const navLinks = document.querySelectorAll('.topnav a');
+
+// Add click event listener to each link
+navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+    // Remove active class from all links
+    navLinks.forEach(link => link.classList.remove('active'));
+    
+    // Add active class to clicked link
+    this.classList.add('active');
+    console.log(this);
+    if (this.getAttribute('href') === '#results') {
+        document.getElementById("resultBlock").style.display = "";
+        document.getElementById("codeBlock").style.display = "none";
+    } else {
+        document.getElementById("codeBlock").style.display = "";
+        document.getElementById("resultBlock").style.display = "none";
+    }
+    });
+});
+
+// Set initial active link (optional)
+document.querySelector('.topnav a').classList.add('active');
+
+const runQueryButton = document.getElementById('runQueryButton');
+if (runQueryButton){
+    document.getElementById('runQueryButton').addEventListener('click', function() {
+        document.getElementById("runQueryButton").disabled = true;
+        vscode.postMessage({
+            command: 'runBigQueryJob'
+        });
+    });
+}
+
+const cancelBigQueryJobButton = document.getElementById('cancelBigQueryJobButton');
+if (cancelBigQueryJobButton){
+    document.getElementById('cancelBigQueryJobButton').addEventListener('click', function() {
+        vscode.postMessage({
+            command: 'cancelBigQueryJob'
+        });
+    });
+}
+
+const queryLimit = document.getElementById('queryLimit');
+if (queryLimit){
+    document.getElementById("queryLimit").addEventListener("change", function() {
+    var selectedValue = this.value;
+    vscode.postMessage({
+        command: 'queryLimit',
+        value: selectedValue
+    });
+    });
+}
 
 function updateDateTime(elapsedTime, totalGbBilled) {
     const now = new Date();
@@ -12,42 +69,40 @@ function updateDateTime(elapsedTime, totalGbBilled) {
       second: '2-digit'
     };
     let queryStatsText = now.toLocaleString('en-US', options) + ' | Took:  (' + elapsedTime + ' seconds) ' + ' | GB billed:  ' + totalGbBilled ;
-    document.getElementById('datetime').textContent = queryStatsText;
+    document.getElementById('datetime').textContent = "Query results ran at: " + queryStatsText;
 }
-
-// Create a loading message element
-const loadingMessage = document.createElement('div');
-loadingMessage.id = 'loading-message';
-loadingMessage.textContent = 'Loading data...';
-loadingMessage.style.cssText = `
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    font-size: 12px;
-`;
-
-let startTime = Date.now();
-let elapsedTime = 0;
-
-function updateLoadingMessage() {
-    elapsedTime = Math.floor((Date.now() - startTime) / 1000);
-    loadingMessage.textContent = `Loading data... (${elapsedTime} seconds)`;
-    return elapsedTime;
-}
-
-elapsedTime = updateLoadingMessage();
-const timerInterval = setInterval(updateLoadingMessage, 1000);
-document.body.appendChild(loadingMessage);
 
 // Hide the table initially
-document.getElementById('bigqueryResults').style.display = 'none';
+const bigQueryResults = document.getElementById('bigqueryResults');
+if (bigQueryResults){
+    bigQueryResults.style.display = 'none';
+}
+
+let timerInterval = undefined;
+let elapsedTime = 0;
+let loadingMessage = undefined;
+
+function postRunCleanup(){
+    clearInterval(timerInterval);
+    if (loadingMessage){
+        document.body.removeChild(loadingMessage);
+    }
+    updateDateTime(elapsedTime, '0');
+    document.getElementById("cancelBigQueryJobButton").disabled = true;
+}
 
 window.addEventListener('message', event => {
     const results = event?.data?.results;
     const columns = event?.data?.columns;
     const jobStats = event?.data?.jobStats;
-    const totalBytesBilled = jobStats.totalBytesBilled;
+    const noResults = event?.data?.noResults;
+    const totalBytesBilled = jobStats?.totalBytesBilled;
+    const bigQueryJobId = event?.data?.bigQueryJobId;
+    const bigQueryJobCancelled = event?.data?.bigQueryJobCancelled;
+    const errorMessage = event?.data?.errorMessage;
+    const query = event?.data?.query;
+    const showLoadingMessage = event?.data?.showLoadingMessage;
+
     let totalGbBilled =  (parseFloat(totalBytesBilled) / 10 ** 9).toFixed(3) + " GB";
 
     if (results && columns) {
@@ -71,5 +126,64 @@ window.addEventListener('message', event => {
             paginationSize:20,
             paginationCounter:"rows",
         });
+    }
+
+    if (bigQueryJobCancelled && bigQueryJobId){
+        postRunCleanup();
+        const jobCancelled = document.querySelector('.bigquery-job-cancelled');
+        if(jobCancelled){
+            jobCancelled.textContent = `❕ BigQuery Job was cancelled, jobId: ${bigQueryJobId}`;
+        }
+    }
+
+    if(noResults){
+        postRunCleanup();
+        const noResultsForQuery = document.getElementById('no-results');
+        if (noResultsForQuery){
+            noResultsForQuery.textContent = `❕ There is no data to display`;
+        }
+    }
+
+    if (query){
+        // document.getElementById('bigqueryerror').textContent = errorMessage;
+        document.getElementById("sqlCodeBlock").textContent = query;
+        hljs.addPlugin( new CopyButtonPlugin({
+            autohide: false, // Always show the copy button
+        }));
+        hljs.highlightAll();
+        hljs.initLineNumbersOnLoad();
+        document.getElementById("codeBlock").style.display = "none";
+    }
+
+    if (errorMessage){
+        postRunCleanup();
+        document.getElementById('bigqueryerror').textContent = errorMessage;
+    }
+
+    if (showLoadingMessage){
+        document.getElementById("cancelBigQueryJobButton").disabled = false;
+        // Create a loading message element
+        loadingMessage = document.createElement('div');
+        loadingMessage.id = 'loading-message';
+        loadingMessage.textContent = 'Loading data...';
+        loadingMessage.style.cssText = `
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 12px;
+        `;
+
+        let startTime = Date.now();
+
+        function updateLoadingMessage() {
+            elapsedTime = Math.floor((Date.now() - startTime) / 1000);
+            loadingMessage.textContent = `Loading data... (${elapsedTime} seconds)`;
+            return elapsedTime;
+        }
+
+        elapsedTime = updateLoadingMessage();
+        timerInterval = setInterval(updateLoadingMessage, 1000);
+        document.body.appendChild(loadingMessage);
     }
 });
--- src/views/register-query-results-panel.ts
+++ src/views/register-query-results-panel.ts
@@ -28,7 +28,7 @@ export class CustomViewProvider implements vscode.WebviewViewProvider {
           await this.updateContent(this._query);
         }
       }else {
-        webviewView.webview.html = this._getHtmlForWebviewGeneric(webviewView.webview);
+        webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
       }
 
       webviewView.onDidChangeVisibility(() => {
@@ -45,8 +45,8 @@ export class CustomViewProvider implements vscode.WebviewViewProvider {
                 let resp = await cancelBigQueryJob();
                 cancelBigQueryJobSignal = false;
                 if (resp.cancelled && this._view){
-                  this._view.webview.html = this._getHtmlForWebviewJobCancelled(this._view.webview);
-                  this._view.webview.postMessage({"bigQueryJobId": resp.bigQueryJobId});
+                  this._view.webview.html = this._getHtmlForWebview(this._view.webview);
+                  this._view.webview.postMessage({"bigQueryJobId": resp.bigQueryJobId, "bigQueryJobCancelled": true});
                   this._view.show(true);
                 }
                 return;
@@ -77,182 +77,101 @@ export class CustomViewProvider implements vscode.WebviewViewProvider {
     }
       try {
           this._view.webview.html = this._getHtmlForWebview(this._view.webview);
+          this._view.webview.postMessage({"showLoadingMessage": true });
           const { results, columns, jobStats } = await queryBigQuery(query);
           if(results){
             this._cachedResults = { results, columns, jobStats };
-            this._view.webview.postMessage({"results": results, "columns": columns, "jobStats": jobStats });
+            this._view.webview.postMessage({"results": results, "columns": columns, "jobStats": jobStats, "query": query });
             //TODO: This needs be before we run the query in backend
             this._view.show(true);
           }else{
             //TODO: even when there is no results we could shows billed bytes 
-            this._view.webview.html = this._getHtmlForWebviewNoResultsToDisplay(this._view.webview);
+            this._view.webview.html = this._getHtmlForWebview(this._view.webview);
+            this._view.webview.postMessage({"noResults": true, "query": query });
             this._view.show(true);
           }
       } catch (error:any) {
         let errorMessage = error?.message;
         if(errorMessage){
-          this._view.webview.html = this._getHtmlForWebviewError(this._view.webview);
-          this._view.webview.postMessage({"errorMessage": errorMessage });
+          this._view.webview.html = this._getHtmlForWebview(this._view.webview);
+          this._view.webview.postMessage({"errorMessage": errorMessage, "query": query });
           this._view.show(true);
         }
       }
   }
 
-  private _getHtmlForWebviewGeneric(webview: vscode.Webview) {
-    const styleResetUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, "media", "css", "query.css"));
-    const showBigQueryGenericScriptUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, "media", "js", "showQueryGeneric.js"));
-    const nonce = getNonce();
-    return /*html*/ `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="${styleResetUri}" rel="stylesheet">
-      </head>
-      <body>
-        <div class="beta-button-container">
-               <button class="beta-button" disabled>BETA</button>
-        </div>
-        <p class="top-left">Query results will be dispalyed here</p>
-        <select id="queryLimit">
-          <option value="1000" selected>Limit: 1000</option>
-          <option value="2000">Limit: 2000</option>
-          <option value="5000">Limit: 5000</option>
-        </select>
-        <button id="runQueryButton" class="runQueryButton" title="Runs current file">RUN</button>
-        <script nonce="${nonce}" type="text/javascript" src="${showBigQueryGenericScriptUri}"></script>
-      </body>
-      </html>
-    `;
-  }
-
-  private _getHtmlForWebviewJobCancelled(webview: vscode.Webview) {
-    const styleResetUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, "media", "css", "query.css"));
-    const showBigQueryGenericScriptUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, "media", "js", "showQueryGeneric.js"));
-    const nonce = getNonce();
-    return /*html*/ `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="${styleResetUri}" rel="stylesheet">
-      </head>
-      <body>
-        <div class="beta-button-container">
-               <button class="beta-button" disabled>BETA</button>
-        </div>
-        <span class="bigquery-job-cancelled"></span>
-        <select id="queryLimit">
-          <option value="1000" selected>Limit: 1000</option>
-          <option value="2000">Limit: 2000</option>
-          <option value="5000">Limit: 5000</option>
-        </select>
-        <button id="runQueryButton" class="runQueryButton">RUN</button>
-        <script nonce="${nonce}" type="text/javascript" src="${showBigQueryGenericScriptUri}"></script>
-      </body>
-      </html>
-    `;
-  }
-
-  private _getHtmlForWebviewNoResultsToDisplay(webview: vscode.Webview) {
-    const styleResetUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, "media", "css", "query.css"));
-    const showBigQueryGenericScriptUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, "media", "js", "showQueryGeneric.js"));
-    const nonce = getNonce();
-    return /*html*/ `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="${styleResetUri}" rel="stylesheet">
-      </head>
-      <body>
-        <div class="beta-button-container">
-               <button class="beta-button" disabled>BETA</button>
-        </div>
-        <span class="warning">❕ There is no data to display</span>
-        <select id="queryLimit">
-          <option value="1000" selected>Limit: 1000</option>
-          <option value="2000">Limit: 2000</option>
-          <option value="5000">Limit: 5000</option>
-        </select>
-        <button id="runQueryButton" class="runQueryButton">RUN</button>
-        <script nonce="${nonce}" type="text/javascript" src="${showBigQueryGenericScriptUri}"></script>
-      </body>
-      </html>
-    `;
-  }
-
-  private _getHtmlForWebviewError(webview: vscode.Webview) {
-    const showBigQueryGenericScriptUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, "media", "js", "showQueryGeneric.js"));
-    const styleResetUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, "media", "css", "query.css"));
-    const nonce = getNonce();
-    return /*html*/ `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="${styleResetUri}" rel="stylesheet">
-
-      </head>
-      <body>
-        <div class="beta-button-container">
-               <button class="beta-button" disabled>BETA</button>
-        </div>
-        <select id="queryLimit">
-          <option value="1000" selected>Limit: 1000</option>
-          <option value="2000">Limit: 2000</option>
-          <option value="5000">Limit: 5000</option>
-        </select>
-        <button id="runQueryButton" class="runQueryButton">RUN</button>
-        <p class="top-left" style="color: red"><span id="bigqueryerror"></span></p>
-        <script nonce="${nonce}" type="text/javascript" src="${showBigQueryGenericScriptUri}"></script>
-      </body>
-      </html>
-    `;
-  }
-
-
-
   private _getHtmlForWebview(webview: vscode.Webview) {
-      const jqueryMinified = webview.asWebviewUri(Uri.joinPath(this._extensionUri, "media", "js", "deps", "jquery-3.7.1.slim.min.js"));
-      const showBigQueryGenericScriptUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, "media", "js", "showQueryGeneric.js"));
-      const showQueryResultsScriptUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, "media", "js", "showQueryResults.js"));
-      const styleResetUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, "media", "css", "query.css"));
-      const nonce = getNonce();
+    const showQueryResultsScriptUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, "media", "js", "showQueryResults.js"));
+    const styleResetUri = webview.asWebviewUri(Uri.joinPath(this._extensionUri, "media", "css", "query.css"));
+    const highlightJsCssUri = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css";
+    const highlightJsUri = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js";
+    const highlightJsCopyExtUri = "https://unpkg.com/highlightjs-copy/dist/highlightjs-copy.min.js";
+    const highlightJsCopyExtCssUri = "https://unpkg.com/highlightjs-copy/dist/highlightjs-copy.min.css";
+    const highlightJsOneDarkThemeUri = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/atom-one-dark.min.css";
+    const highlightJsLineNoExtUri = "https://cdn.jsdelivr.net/npm/highlightjs-line-numbers.js/dist/highlightjs-line-numbers.min.js";
+    const tabulatorCssUri = "https://unpkg.com/tabulator-tables@6.2.5/dist/css/tabulator.min.css";
+    const tabulatorUri = "https://unpkg.com/tabulator-tables@6.2.5/dist/js/tabulator.min.js";
+    const nonce = getNonce();
 
     return /*html*/ `
       <!DOCTYPE html>
       <html lang="en">
       <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link rel="stylesheet" href="${highlightJsCssUri}">
+          <script src="${highlightJsUri}"></script>
+          <script src="${highlightJsCopyExtUri}"></script>
+          <link rel="stylesheet" href="${highlightJsCopyExtCssUri}" />
+          <link rel="stylesheet" href="${highlightJsOneDarkThemeUri}">
+          <script src="${highlightJsLineNoExtUri}"></script>
 
-        <script nonce="${nonce}" type="text/javascript" src="${jqueryMinified}"></script>
-        <link href="https://unpkg.com/tabulator-tables@6.2.5/dist/css/tabulator.min.css" rel="stylesheet">
-        <script type="text/javascript" src="https://unpkg.com/tabulator-tables@6.2.5/dist/js/tabulator.min.js"></script>
-        <link href="${styleResetUri}" rel="stylesheet">
+          <link href="${tabulatorCssUri}" rel="stylesheet">
+          <script type="text/javascript" src="${tabulatorUri}"></script>
+
+          <link href="${styleResetUri}" rel="stylesheet">
+          <style>
+      </style>
       </head>
+
       <body>
-        <div class="beta-button-container">
-               <button class="beta-button" disabled>BETA</button>
-        </div>
-        <select id="queryLimit">
-          <option value="1000" selected>Limit: 1000</option>
-          <option value="2000">Limit: 2000</option>
-          <option value="5000">Limit: 5000</option>
-        </select>
-        <button id="runQueryButton" class="runQueryButton">RUN</button>
-        <button id="cancelBigQueryJobButton" class="cancelBigQueryJobButton">Cancel query</button>
-        <p class="top-left">Query results ran at: <span id="datetime"></span></p>
-        <table id="bigqueryResults" class="display" width="100%"></table>
-        <script nonce="${nonce}" type="text/javascript" src="${showBigQueryGenericScriptUri}"></script>
+
+      <div class="topnav">
+        <a class="active" href="#results">Results</a>
+        <a href="#query">Query</a>
+      </div>
+
+      <div class="beta-button-container">
+        <button class="beta-button" disabled>BETA</button>
+      </div>
+
+      <span class="bigquery-job-cancelled"></span>
+
+      <select id="queryLimit">
+        <option value="1000" selected>Limit: 1000</option>
+        <option value="2000">Limit: 2000</option>
+        <option value="5000">Limit: 5000</option>
+      </select>
+
+      <button id="runQueryButton" class="runQueryButton">RUN</button>
+      <button id="cancelBigQueryJobButton" class="cancelBigQueryJobButton">Cancel query</button>
+
+      <p><span id="datetime"></span></p>
+      <p><span id="no-results"></span></p>
+
+      <div id="codeBlock">
+        <pre><code  id="sqlCodeBlock" class="language-sql"></code></pre>
         <script nonce="${nonce}" type="text/javascript" src="${showQueryResultsScriptUri}"></script>
+      </div>
+
+      <div id="resultBlock">
+        <p  style="color: red"><span id="bigqueryerror"></span></p>
+        <table id="bigqueryResults" class="display" width="100%"></table>
+      </div>
+
       </body>
       </html>
     `;
   }
+
 }
