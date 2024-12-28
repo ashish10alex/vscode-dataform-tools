d19058229f36b47ca08afe71b21a18ec43f4a1be
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2024-12-28 23:13:19 +0000

    chore: one more test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -59,6 +59,39 @@ suite('GetMetadataForSqlxFileBlocks', () => {
         }
     });
 
+     test('Single line config / pre / post operation blocks', async () => {
+        try {
+
+            const workspacePath = path.resolve(__dirname, '..', '..', '..', 'src', 'test', 'test-workspace');
+            const uri = vscode.Uri.file(path.join(workspacePath, 'definitions/010_SINGLE_LINE.sqlx'));
+            let doc = await vscode.workspace.openTextDocument(uri);
+            assert.ok(doc);
+            let sqlxBlockMetadata = getMetadataForSqlxFileBlocks(doc);
+
+            /**config block */
+            assert.strictEqual(sqlxBlockMetadata.configBlock.startLine, 1);
+            assert.strictEqual(sqlxBlockMetadata.configBlock.endLine, 1);
+
+            /** Pre ops block */
+            assert.strictEqual(sqlxBlockMetadata.preOpsBlock.preOpsList.length, 1);
+            assert.strictEqual(sqlxBlockMetadata.preOpsBlock.preOpsList[0].startLine, 3);
+            assert.strictEqual(sqlxBlockMetadata.preOpsBlock.preOpsList[0].endLine, 3);
+
+            /** Post ops block */
+            assert.strictEqual(sqlxBlockMetadata.postOpsBlock.postOpsList.length, 1);
+            assert.strictEqual(sqlxBlockMetadata.postOpsBlock.postOpsList[0].startLine, 5);
+            assert.strictEqual(sqlxBlockMetadata.postOpsBlock.postOpsList[0].endLine, 5);
+
+            /** sql block */
+            assert.strictEqual(sqlxBlockMetadata.sqlBlock.startLine, 7);
+            assert.strictEqual(sqlxBlockMetadata.sqlBlock.endLine, 7);
+        } catch (error: any) {
+            console.error('Test failed:', error);
+            vscode.window.showErrorMessage(`Test failed: ${error.message}`);
+            throw error;
+        }
+    });
+
 });
 
 suite('setDiagnostics', () => {
