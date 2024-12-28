80bbde233de0a5d3c30a6c4680e05dde41b55e26
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2024-12-28 22:52:14 +0000

    feat: add one more test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -31,4 +31,31 @@ suite('GetMetadataForSqlxFileBlocks', () => {
         assert.strictEqual(sqlxBlockMetadata.sqlBlock.startLine, 15);
         assert.strictEqual(sqlxBlockMetadata.sqlBlock.endLine, 19);
     });
+
+
+    test('Unnested config block and sql block', async function() {
+        this.timeout(9000);
+        try {
+            const workspacePath = path.resolve(__dirname, '..', '..', '..', 'src', 'test', 'test-workspace');
+            const uri = vscode.Uri.file(path.join(workspacePath, 'definitions/0100_TEST.sqlx'));
+            console.log('[TEST] URI:', uri.toString());
+            let doc = await vscode.workspace.openTextDocument(uri);
+            assert.ok(doc);
+            let sqlxBlockMetadata = getMetadataForSqlxFileBlocks(doc);
+
+            assert.strictEqual(sqlxBlockMetadata.configBlock.startLine, 1);
+            assert.strictEqual(sqlxBlockMetadata.configBlock.endLine, 6);
+            assert.strictEqual(sqlxBlockMetadata.sqlBlock.startLine, 9);
+            assert.strictEqual(sqlxBlockMetadata.sqlBlock.endLine, 45);
+            assert.strictEqual(sqlxBlockMetadata.postOpsBlock.postOpsList.length, 0);
+            assert.strictEqual(sqlxBlockMetadata.preOpsBlock.preOpsList.length, 0);
+
+        } catch (error: any) {
+            console.error('Test failed:', error);
+            vscode.window.showErrorMessage(`Test failed: ${error.message}`);
+            throw error;
+        }
+    });
+
 });
+
