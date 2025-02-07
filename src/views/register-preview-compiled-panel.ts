9e085fdf0b0670f43809948a17904e8f02e9b750
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2025-02-07 16:58:18 +0000

    fix: copy model button not showing

--- src/views/register-preview-compiled-panel.ts
+++ src/views/register-preview-compiled-panel.ts
@@ -574,7 +574,10 @@ export class CompiledQueryPanel {
             <a id="targetTableOrViewLink"></a>
             <div class="copy-button-container">
                 <button id="copyModelNameButton" class="copy-model-button" title="Copy model name">
-                    <i class="fa-regular fa-copy"></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+                        <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+                    </svg>
                     <span class="button-text"></span>
                 </button>
             </div>
