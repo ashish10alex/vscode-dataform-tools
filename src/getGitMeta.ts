27c3f7911836bf602708aa16e63040844a546bcc
Ashish Alex <ashish.alex10@gmail.com>
2025-10-13 18:10:38 +0100

    chore: cleanup function that gets git branch and repo name

--- src/getGitMeta.ts
+++ src/getGitMeta.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode';
 
 export async function getGitBranchAndRepoName() {
-  // Get the Git extension
   const gitExtension = vscode.extensions.getExtension('vscode.git')?.exports;
   if (!gitExtension) {
     vscode.window.showErrorMessage('Git extension not found.');
@@ -15,13 +14,9 @@ export async function getGitBranchAndRepoName() {
     return;
   }
 
-  // Use the first repository for this example
   const repo = git.repositories[0];
-
-  // Get current branch name
   const gitBranch = repo.state.HEAD?.name ?? 'No branch';
-
-    const gitRepoName = repo.state.remotes[0].fetchUrl.split("/").pop().split(".")[0];
+  const gitRepoName = repo.state.remotes[0].fetchUrl.split("/").pop().split(".")[0];
 
   return { gitBranch, gitRepoName };
 }
