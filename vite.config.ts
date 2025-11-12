68eda8faaba8772a377b56ee18b4c42a4b4e9c33
Ashish Alex <ashish.alex10@gmail.com>
2025-11-12 22:40:36 +0000

    fix(webview): dist folder not getting created by prepublish in ci (#209)

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
       },
     },
     sourcemap: true,
-    watch: {}
+    // watch: {}
   },
   css: {
     postcss: './postcss.config.js'
@@ -24,4 +24,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './webviews/dependancy_graph')
     }
   }
-}); 
+});
