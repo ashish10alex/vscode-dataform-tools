cfeabeab03b51eae7d43f48507d468cc72ee4f07
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2026-03-05 18:12:07 +0000

    fix: do not overide projectId for past workflow executions

--- src/views/register-preview-compiled-panel.ts
+++ src/views/register-preview-compiled-panel.ts
@@ -445,9 +445,7 @@ export class CompiledQueryPanel {
                     const updatedUrls = await Promise.all(urlsToRefresh.map(async (item) => {
                         if (item.state !== 'SUCCEEDED' && item.state !== 'FAILED' && item.state !== 'CANCELLED' && item.workflowInvocationId && item.projectId && item.location && item.repositoryName) {
                             try {
-                                const gcpProjectIdOveride = vscode.workspace.getConfiguration('vscode-dataform-tools').get('gcpProjectId');
-                                const projectId = (gcpProjectIdOveride || item.projectId) as string;
-                                const dataformClient = new DataformTools(projectId, item.location);
+                                const dataformClient = new DataformTools(item.projectId, item.location);
                                 const invocation = await dataformClient.getWorkflowInvocation(item.repositoryName, item.workflowInvocationId);
                                 if (invocation && invocation.state) {
                                   item.state = invocation.state as string;
