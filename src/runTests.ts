3fd0129753b513efd92622d0062994282712206b
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2026-03-16 15:49:48 +0000

    feat: get workspace when not available and use custom dataform cli path if specified

--- src/runTests.ts
+++ src/runTests.ts
@@ -1,20 +1,21 @@
 import * as vscode from 'vscode';
-import { getDataformCliCmdBasedOnScope, getDataformCompilationTimeoutFromConfig, runCommandInTerminal } from "./utils";
+import { getDataformCliCmdBasedOnScope, getDataformCompilationTimeoutFromConfig, getWorkspaceFolder, runCommandInTerminal } from "./utils";
 
-export async function runTests(workspaceFolder: string) {
-    if (!workspaceFolder) {
+export async function runTests(workspaceFolder?: string) {
+    const resolvedWorkspaceFolder = workspaceFolder ?? await getWorkspaceFolder();
+    if (!resolvedWorkspaceFolder) {
         vscode.window.showErrorMessage("Unable to run tests: Workspace folder could not be determined.");
         return;
     }
     let dataformCompilationTimeoutVal = getDataformCompilationTimeoutFromConfig();
-    const customDataformCliPath = getDataformCliCmdBasedOnScope(workspaceFolder);
+    const customDataformCliPath = getDataformCliCmdBasedOnScope(resolvedWorkspaceFolder);
     
     if (dataformCompilationTimeoutVal) {
         dataformCompilationTimeoutVal = `--timeout=${dataformCompilationTimeoutVal}`;
     } else {
         dataformCompilationTimeoutVal = "";
     }
-    let cmd = `${customDataformCliPath} test "${workspaceFolder}" ${dataformCompilationTimeoutVal}`;
+    let cmd = `${customDataformCliPath} test "${resolvedWorkspaceFolder}" ${dataformCompilationTimeoutVal}`;
     
     runCommandInTerminal(cmd);
 }
