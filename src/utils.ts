d3a0b0b4080361d3a6f10fb13f51d8d4dd533923
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2026-03-25 14:00:30 +0000

    chore: migrate offset calculation to a separate file

--- src/utils.ts
+++ src/utils.ts
@@ -17,6 +17,7 @@ import { DataformTools } from "@ashishalex/dataform-tools";
 import { sendWorkflowInvocationNotification, syncAndrunDataformRemotely } from "./dataformApiUtils";
 import { GitService } from './gitClient';
 import { load as loadYaml, YAMLException } from 'js-yaml';
+import { calculateIncrementalPreOpsOffset } from './offsetCalculations';
 
 let supportedExtensions = ['sqlx', 'js', 'yaml', 'json'];
 
@@ -2138,25 +2139,11 @@ export async function dryRunAndShowDiagnostics(curFileMeta: any, document: vscod
             let compiledPreOpsLineCount: number | undefined = undefined;
             if (type === "incremental" && !skipPreOpsInDryRun) {
                 const iq = fileMetadata.queryMeta.incrementalQueries[0];
-                if (iq?.incrementalPreOpsQuery) {
-                    // N_p: lines occupied by pre-ops in the combined dry-run query (p + "\n" + incrementalQuery).
-                    // p.split('\n').length = iq.incrementalPreOpsQuery.split('\n').length (adding ";" doesn't add \n).
-                    const N_p = iq.incrementalPreOpsQuery.split('\n').length;
-
-                    // N_inc_preamble: leading blank lines in incrementalQuery that Dataform adds before
-                    // the actual SQL content. These shift BigQuery error line numbers but are NOT in
-                    // the .sqlx editor file, so they must be subtracted when mapping back to editor lines.
-                    const incQueryLines = (iq.incrementalQuery || '').split('\n');
-                    let N_inc_preamble = 0;
-                    for (const line of incQueryLines) {
-                        if (line.trim() === '') { N_inc_preamble++; } else { break; }
-                    }
-
-                    // setDiagnostics uses: preOpsOffset = compiledPreOpsLineCount + 2
-                    // We need: preOpsOffset = N_p + N_inc_preamble + 2 - offSet
-                    // (the -offSet cancels the offSet term in the formula, which also accounts for preamble)
-                    compiledPreOpsLineCount = N_p + N_inc_preamble - offSet;
-                }
+                compiledPreOpsLineCount = calculateIncrementalPreOpsOffset(
+                    iq?.incrementalPreOpsQuery,
+                    iq?.incrementalQuery,
+                    offSet
+                );
             }
 
             setDiagnostics(document, errorMeta, diagnosticCollection, sqlxBlockMetadata, offSet, compiledPreOpsLineCount);
